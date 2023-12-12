37b865f58440b3903b32b5407b9cac0fcbc1feb4
Samir B <smdae99@gmail.com>
2023-12-12 10:46:50 +0100

    Quest 3 done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,26 @@
-function PokemonCard() {
-    return (
-      <figure>
-        <figcaption>Bulbasaur</figcaption>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-      </figure>
-    );
-  }
+const pokemonList = [
+  {
+    name: 'Bulbasaur',
+    imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
+  {
+    name: 'mew',
+  },
+  {
+    name: 'Random',
+    imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png',
+  },
+];
+
+const PokemonCard = () => {
+  let pokemon = pokemonList[0]; 
   
-  export default PokemonCard;
+  return (
+    <div>
+      <h2>{pokemon.name}</h2>
+      {pokemon.imgSrc ? (<img src={pokemon.imgSrc}/>) : (<p>???</p>)}
+    </div>
+  );
+};
+
+export default PokemonCard;
