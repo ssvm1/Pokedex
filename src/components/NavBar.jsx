e10d52898e08a7646f11be518e9be7559faa73b1
Samir B <smdae99@gmail.com>
2023-12-13 13:43:02 +0100

    Quest 8 react

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,21 @@
-function NavBar({setPokemonIndex, pokemonIndex, pokemonList}) {
+function NavBar({ setPokemonIndex, pokemonList }) {
+  const onClick = (index) => {
+    setPokemonIndex(index);
+  };
 
-
-const handleClickNext = () => {
-  console.log("Next click")
-  setPokemonIndex( pokemonIndex + 1)
-}
-const handleClickPrevious = () => {
-  console.log("Previous click")
-  setPokemonIndex( pokemonIndex - 1)
-}
-
-return (
+  return (
     <>
-{pokemonIndex > 0 && (
-    <button onClick={handleClickPrevious}>Précédent</button>
-  )}
-  {pokemonIndex < pokemonList.length - 1 && (
-    <button onClick={handleClickNext}>Suivant</button>
-  )}
-  </>
-)
+      <ul>
+        {pokemonList.map((pokemon, PokemonListIndex) => (
+          <li key={PokemonListIndex}>
+            <button onClick={() => onClick(PokemonListIndex, pokemon.name)}>
+              {pokemon.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default NavBar;
